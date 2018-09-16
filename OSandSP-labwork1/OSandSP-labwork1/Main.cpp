d491f7afc10b64ec7ee1b6f94aa36d6ada86db56
Nadya Povalyaeva <iriejamaica@outlook.com>
2018-09-16 22:36:16 +0300

    Add interaction with the keyboard

--- OSandSP-labwork1/OSandSP-labwork1/Main.cpp
+++ OSandSP-labwork1/OSandSP-labwork1/Main.cpp
@@ -1,5 +1,7 @@
 #include <windows.h> //Содержит все базовые функции WinAPI
 #include <tchar.h>
+
+
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 TCHAR WinClassName[] = _T("MainClass");
 
@@ -32,7 +34,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, // Описатель (дескриптор) загруженно
 
 	// Определение класса окна
 	wcex.cbSize = sizeof(WNDCLASSEX); //Указывается для удобства последующей сериализации
-	wcex.style = CS_HREDRAW | CS_VREDRAW |  CS_DBLCLKS;
+	wcex.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
 	wcex.lpfnWndProc = WndProc; // Указатель на процедуру, обрабатывающую сообщения
 	wcex.cbClsExtra = 0; // Число дополнительных байтов для оконного класса
 	wcex.cbWndExtra = 0; // Число дополнительных байтов для окна
@@ -43,8 +45,6 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, // Описатель (дескриптор) загруженно
 	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); // Заполнение окна белым цветом
 	wcex.lpszMenuName = NULL; // Нет строки меню
 	wcex.lpszClassName = WinClassName; // Макрос _T() служит для создания строки Unicode
-
-
 
 	RegisterClassEx(&wcex); // Регистрация класса окна
 
@@ -74,56 +74,134 @@ int APIENTRY _tWinMain(HINSTANCE hInstance, // Описатель (дескриптор) загруженно
 	return 0;
 }
 
+HDC hdc; // Дескриптор контекста устройства
+PAINTSTRUCT ps;
+RECT figureRt, backRt;
+bool firstWinSizeFlag = true;
+int rectX1, rectY1, rectX2, rectY2;
+int windowX, windowY;
+//HPEN rectanglePen, backgroundPen;
+HBRUSH rectangleBrush, backgroundBrush;
+COLORREF backgroundColor = RGB(231, 231, 231), rectangleColor = RGB(1, 90, 91);
 
-// Обработчик сообщений
+void FindRectangleSize(int winX, int winY, int &x1, int &y1, int &x2, int &y2)
+{
+	x1 = winX * 3 / 8;
+	y1 = winY * 3 / 8;
+	x2 = winX * 5 / 8;
+	y2 = winY * 5 / 8;
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	HDC hdc; //Дескриптор контекста устройства
-	PAINTSTRUCT ps;
-
-	static int sx, sy;
-	static HPEN rectanglePen, backgroundPen;
-	static HBRUSH rectangleBrush, backgroundBrush;
-	COLORREF backgroundColor = RGB(231, 231, 231), rectangleColor = RGB(1, 90, 91);
-
 	switch (message)
 	{
 		case WM_CREATE:
-			rectanglePen = CreatePen(PS_SOLID, 1, rectangleColor); // Создание пера
-			backgroundPen = CreatePen(PS_SOLID, 1, backgroundColor);
-			rectangleBrush = CreateSolidBrush(rectangleColor); // Создание кисти
+		{
+			//rectanglePen = CreatePen(PS_SOLID, 1, rectangleColor); // Cоздание пера
+			//backgroundPen = CreatePen(PS_SOLID, 1, backgroundColor);
+			rectangleBrush = CreateSolidBrush(rectangleColor); // Cоздание кисти
 			backgroundBrush = CreateSolidBrush(backgroundColor);
-			break;
+		}
+		break;
 		case WM_SIZE:
-			sx = LOWORD(lParam); // Ширина окна
-			sy = HIWORD(lParam); // Высота окна
-			break;
-		case WM_PAINT: // Заливка фона и создание спрайта
-
-			//Посмоти, как это можно оптимизировать
-
+		{
+			windowX = LOWORD(lParam); // Ширина окна
+			windowY = HIWORD(lParam); // Высота окна
+			if (firstWinSizeFlag) 			
+			{
+				FindRectangleSize(windowX, windowY, rectX1, rectY1, rectX2, rectY2);
+				firstWinSizeFlag = false;
+			}
+		}
+		break;
+		case WM_PAINT:
+		{
 			hdc = BeginPaint(hWnd, &ps);
-			SelectObject(hdc, backgroundBrush);
-			Rectangle(hdc, 0, 0, sx, sy); // (x1, y1) — координаты левого верхнего угла прямоугольника, (x2, y2) — координаты правого нижнего угла
-			SelectObject(hdc, backgroundPen);
-			Rectangle(hdc, 0, 0, sx, sy);
-
-			SelectObject(hdc, rectangleBrush);
-			Rectangle(hdc, sx * 3 / 8, sy * 3 / 8, sx * 5 / 8, sy * 5 / 8); 
-			SelectObject(hdc, rectanglePen);
-			Rectangle(hdc, sx * 3 / 8, sy * 3 / 8, sx * 5 / 8, sy * 5 / 8);
+			//SelectObject(hdc, backgroundBrush);
+			//Rectangle(hdc, 0, 0, windowX, windowY); // (x1, y1) — координаты левого верхнего угла прямоугольника, (x2, y2) — координаты правого нижнего угла
+			//SelectObject(hdc, backgroundPen);
+			//Rectangle(hdc, 0, 0, windowX, windowY);
+			//SelectObject(hdc, rectangleBrush);
+			//Rectangle(hdc, rectX1, rectY1, rectX2, rectY2);
+			//SelectObject(hdc, rectanglePen);
+			//Rectangle(hdc, rectX1, rectY1, rectX2, rectY2);
+			SetRect(&backRt, 0, 0, windowX, windowY);
+			FillRect(hdc, &backRt, backgroundBrush);
+			SetRect(&figureRt, rectX1, rectY1, rectX2, rectY2);
+			FillRect(hdc, &figureRt, rectangleBrush);
 			EndPaint(hWnd, &ps);
-			break;
+		}
+		break;
+		case WM_KEYDOWN:
+		{
+
+			//https://docs.microsoft.com/en-us/windows/desktop/inputdev/virtual-key-codes
+
+			switch (wParam)
+			{
+				case VK_RIGHT:
+				{
+					if (rectX2 < windowX)
+					{
+						rectX1 += 3; rectX2 += 3;
+						SetRect(&figureRt, rectX1, rectY1, rectX2, rectY2);
+						FillRect(hdc, &figureRt, rectangleBrush);
+						InvalidateRect(hWnd, NULL, TRUE);
+						//UpdateWindow(hWnd);
+					}
+				}
+				break;
+				case VK_LEFT:
+					if (rectX1 > 0)
+					{
+						rectX1 -= 3; rectX2 -= 3;
+						SetRect(&figureRt, rectX1, rectY1, rectX2, rectY2);
+						FillRect(hdc, &figureRt, rectangleBrush);
+						InvalidateRect(hWnd, NULL, TRUE);
+						//UpdateWindow(hWnd);
+					}
+					break;
+				case VK_UP:
+				{
+					if (rectY1 > 0)
+					{
+						rectY1 -= 3; rectY2 -= 3;
+						SetRect(&figureRt, rectX1, rectY1, rectX2, rectY2);
+						FillRect(hdc, &figureRt, rectangleBrush);
+						InvalidateRect(hWnd, NULL, TRUE);
+						//UpdateWindow(hWnd);
+					}
+				}
+				break;
+				case VK_DOWN:
+				{
+					if (rectY2 < windowY)
+					{
+						rectY1 += 3; rectY2 += 3;
+						SetRect(&figureRt, rectX1, rectY1, rectX2, rectY2);
+						FillRect(hdc, &figureRt, rectangleBrush);
+						InvalidateRect(hWnd, NULL, TRUE);
+						//UpdateWindow(hWnd);
+					}
+				}
+				break;
+				default:
+					return DefWindowProc(hWnd, message, wParam, lParam);
+			}
+		}
+		break;
 		case WM_DESTROY:
+		{
 			// Удаление созданных объектов
-			DeleteObject(backgroundPen); 
-			DeleteObject(rectanglePen);
+			//DeleteObject(backgroundPen);
+			//DeleteObject(rectanglePen);
 			DeleteObject(backgroundBrush);
 			DeleteObject(rectangleBrush);
 			PostQuitMessage(0);
-			break; // Завершение программы
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+		default: return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 	return 0;
 }
